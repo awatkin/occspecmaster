9d3dd5471320be52259fa2ed28cb3b91277012a8
awatkin <144458571+awatkin@users.noreply.github.com>
2025-02-05 14:28:41 +0000

    registration for initial admin now working.

---
+++ May 24/Solution/admin/adminreg.php
@@ -0,0 +1,47 @@
+<?php
+// file to validate information for new admin user before inserting into database
+
+include '../functs.php';  // brings in the funct file for common functions
+
+$phrase = "@rzl.com";  // used to check correct format of email address
+
+if (strpos($_POST['email'], $phrase) == false) {  // checks for key phrase in email field
+    header("refresh:4; url=one_time_admin_reg.php");
+    echo "<link rel='stylesheet' href='admin_styles.css'>";
+    echo "Invalid email, try again";
+} elseif(pwrd_checker($_POST['password'], $_POST['cpassword']) == false) {  //calls function to check password complexity
+    header("refresh:4; url=one_time_admin_reg.php");
+    echo "<link rel='stylesheet' href='admin_styles.css'>";
+    echo "Password related issue, try again";
+
+} else {
+// this code runs if the previous checks are ok
+
+    include '../dbconnect/db_connect_insert.php';//Used to insert the data into the database, if valid
+    try {  //try this code
+
+        $hpswd = password_hash($_POST['password'], PASSWORD_DEFAULT);  //has the password
+        $sql = "INSERT INTO admin_users (username, password, email, f_name, s_name, signup_date) VALUES (?, ?, ?, ?, ?, ?)";  //prepare the sql to be sent
+        $stmt = $conn->prepare($sql); //prepare to sql
+
+        $stmt->bindParam(1,$_POST['username']);  //bind parameters for security
+        $stmt->bindParam(2,$hpswd);
+        $stmt->bindParam(3,$_POST['email']);
+        $stmt->bindParam(4,$_POST['fname']);
+        $stmt->bindParam(5,$_POST['sname']);
+        $signup_date = time();
+        $stmt->bindParam(6,$signup_date);
+
+        $stmt->execute();  //run the query to insert
+        header("refresh:5; url=admin_login.php"); //confirm and redirect
+        echo "<link rel='stylesheet' href='admin_styles.css'>";
+        echo "Successfully registered";
+    } catch (PDOException $e) { //catch error
+        header("refresh:4; url=one_time_admin_reg.php");
+        echo "<link rel='stylesheet' href='admin_styles.css'>";
+        echo "Error: " . $e->getMessage();
+        echo "Password related issue, try again";
+    }
+
+}
+
--- May 24/Solution/admin/one_time_admin_reg.php
+++ May 24/Solution/admin/one_time_admin_reg.php
@@ -1,3 +1,94 @@
 <?php
 
-// this file is it to create the initial admin account
+//Code to product a one time reg page for admin users
+
+//session_start(); // trys to connect with user session
+
+//checks to see if user exists, then proceeds
+
+
+
+try {  //try this code, catch errors
+    include '../dbconnect/db_connect_select.php';//gets the select user details to check if admin exists or not
+    $sql = "SELECT * FROM admin_users"; //set up the sql statement
+    $stmt = $conn->prepare($sql); //prepares
+    $stmt->execute(); //run the sql code
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);  //brings back results
+
+    if ($result) {  // if there is a result returned
+
+        header("refresh:4; url=admin_login.php");
+        echo "<link rel='stylesheet' href='admin_styles.css'>";
+        echo "ADMIN ALREADY EXISTS, LOGIN or ASK FOR to be registered";
+
+    } else {
+
+        echo "<!DOCTYPE html>";
+
+        echo "<html lang='en'>";
+
+        echo "<head>";
+
+        echo "<link rel='stylesheet' href='admin_styles.css'>";
+
+        echo "<title> RZL One Time Admin Registration</title>";
+
+        echo "</head>";
+
+        echo "<body>";
+
+        echo "<div id='container'>";
+
+        echo "<div id='title'>";
+
+        echo "<h3 id='banner'>RZL Admin one time registration</h3>";
+
+        echo "</div>";
+
+        echo "<div id='navbar'>";
+
+        echo "<ul id='menu'>";
+
+
+        echo "<div id='content'>";
+
+        echo "<h4> This is a one time registration for RZL system</h4>";
+
+        echo "<br>";
+
+        echo "<form method='post' action='adminreg.php'>";
+
+        echo "<input type='text' name='username' placeholder='Username' required><br>";
+
+        echo "<input type='password' name='password' placeholder='Password' required><br>";
+
+        echo "<input type='password' name='cpassword' placeholder='Confirm Password' required><br>";
+
+        echo "<input type='text' name='fname' placeholder='First Name' required><br>";
+
+        echo "<input type='text' name='sname' placeholder='Surname' required><br>";
+
+        echo "<input type='text' name='email' placeholder='Email' required><br>";
+
+        echo "<input type='submit' name='submit' value='Register'>";
+
+        echo "<br><br>";
+
+        echo "</div>";
+
+        echo "</div>";
+
+        echo "</body>";
+
+        echo "</html>";
+
+    }
+} catch(PDOException $e) {
+
+
+    //header("refresh:4; url=admin_login.php");
+    echo "<link rel='stylesheet' href='admin_styles.css'>";
+    echo "ERROR: " . $e->getMessage();
+}
+
+?>
