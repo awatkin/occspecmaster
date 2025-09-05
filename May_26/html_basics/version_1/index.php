e4d32297edc8c0f5ca3ba337bf3f9f478ed9c9b5
awatkin <144458571+awatkin@users.noreply.github.com>
2025-09-05 14:34:31 +0100

    started a html basics folder in may 26 for new year 13

---
+++ May_26/html_basics/version_1/index.php
@@ -0,0 +1,57 @@
+<?php // This open the php code section
+
+echo "<!DOCTYPE html>";
+
+
+echo "<html>";
+
+    echo "<head>";
+
+        echo "<title> watkin</title>";
+
+    echo "</head>";
+
+    echo "<body>";
+
+
+        echo "<h1> Watkin </h1>";
+
+        echo "This is my page.";
+
+        echo "<a href='page2.php'> Go to page 2. </a>";
+
+        echo "<img src='homer.jfif'>";
+
+        echo "<table>";
+
+            echo "<tr>";
+                echo "<td>Hello1</td>";
+                echo "<td>Hello2</td>";
+                echo "<td>Hello3</td>";
+            echo "</tr>";
+            echo "<tr>";
+                echo "<td>Hello4</td>";
+                echo "<td>Hello5</td>";
+                echo "<td>Hello6</td>";
+            echo "</tr>";
+
+
+        echo "</table>";
+
+        echo "<ul>";
+            echo "<li> THis is item 1 </li>";
+            echo "<li> THis is item 2 </li>";
+            echo "<li> THis is item 2 </li>";
+        echo "</ul>";
+
+    echo "<ol>";
+        echo "<li> THis is item 1 </li>";
+        echo "<li> THis is item 2 </li>";
+        echo "<li> THis is item 2 </li>";
+    echo "</ol>";
+
+    echo "</body>";
+
+
+echo "</html>";
+?>
